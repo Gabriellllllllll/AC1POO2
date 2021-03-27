7857330679fd66cc747e6cee152cc564f1125830
GabrielMaciel <gabrielmacielboituva19@gmail.com>
2021-03-27 10:18:08 -0300

    Put Concluido

--- src/main/java/ac1/ac1/controllers/EventController.java
+++ src/main/java/ac1/ac1/controllers/EventController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import ac1.ac1.dto.EventDTO;
 import ac1.ac1.dto.EventInsertDTO;
+import ac1.ac1.dto.EventUpdateDTO;
 import ac1.ac1.services.EventService;
 
 @RestController
@@ -48,5 +50,11 @@ public class EventController {
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventUpdateDTO updateDTO){
+        EventDTO dto = service.update(id,  updateDTO);
+        return ResponseEntity.ok().body(dto);
     }
 }
