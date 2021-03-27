5dd7525e3cd22d616072ebd02c2dd07dd31601d7
GabrielMaciel <gabrielmacielboituva19@gmail.com>
2021-03-27 11:29:30 -0300

    Paginação Concluida

--- src/main/java/ac1/ac1/repositories/EventRepository.java
+++ src/main/java/ac1/ac1/repositories/EventRepository.java
@@ -1,6 +1,9 @@
 package ac1.ac1.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ac1.ac1.entities.Event;
@@ -8,4 +11,6 @@ import ac1.ac1.entities.Event;
 @Repository
 public interface EventRepository extends JpaRepository <Event, Long>{
     
+    @Query("SELECT c FROM Event c " + "WHERE " + "(LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))) AND " + "(LOWER(c.place) LIKE LOWER(CONCAT('%', :place, '%')))")
+    public Page <Event> find(Pageable pageRequest, String name, String place);
 }
